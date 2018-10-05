f3bfc1965918af0b8df3189a29831b7602ad0013
VaibhaviPatil <33600528+VaibhaviPatil@users.noreply.github.com>
2018-10-06 00:02:05 +0530

    Add files via upload

---
+++ vaibhaviFolder/App/sketch_mar24a/sketch_mar24a.ino
@@ -0,0 +1,34 @@
+/* int a[50]={};
+int myArray[10]={9,3,2,4,3,2,7,8,9,11};
+*/
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while(Serial.available())
+{
+  /*
+   *while(Serial.read()=='0');
+  for(int i=0;i<4;i++)
+  {
+   a[i]=Serial.read(); 
+  }
+}
+  for(int i=0;i<4;i++)
+  {
+   Serial.println(a[i]);
+  }
+   */delay(10);
+ char a=Serial.read();
+ Serial.println("Outside");
+ Serial.println(a);
+  if(a!='0')
+  {
+    Serial.println("Inside");
+    Serial.println(a);
+  }
+}
+}
